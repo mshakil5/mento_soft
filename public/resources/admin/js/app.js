dd57bab2f7afa5e3e96345c636f229df801246e1
Sakib <nsakib.miraz@gmail.com>
2025-09-24 16:24:23 +0600

    summernote issue fix

--- public/resources/admin/js/app.js
+++ public/resources/admin/js/app.js
@@ -51,6 +51,8 @@ $(document).ready(function () {
   $('.summernote').summernote({
     height: 200,
     resize: true,
+    dialogsInBody: true,
+    dialogsFade: false,
     fontNamesIgnoreCheck: ['Titillium Web'],
     fontNames: $.summernote.options.fontNames.concat(['Titillium Web']),
   });
